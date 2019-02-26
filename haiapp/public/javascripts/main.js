7bdc07fd2e4de005ecbeaf193a2ab1666bd2abb2
Akhil <akhiledathadan007@gmail.com>
2019-02-26 10:08:38 +0530

    Copied files

---
+++ haiapp/public/javascripts/main.js
@@ -0,0 +1,24 @@
+function Backtohome() {
+    window.location.href = '/';
+}
+
+function about() {
+    window.location.href = '/about';
+}
+
+function alertbox() {
+    alert("hai i am alert box");
+}
+
+function alertboxcall() {
+    window.location.href = '/alertbox';
+}
+
+function formcall() {
+    window.location.href ='/form';
+}
+
+function Submit() {
+    const indata = document.getElementById('submit').value;
+    alert(indata);
+}
